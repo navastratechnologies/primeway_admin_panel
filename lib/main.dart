4b75dbe319ced59119f859ced3b893091eb878c4
Centurus Technologies <centurustechnologies@gmail.com>
2023-07-26 17:42:46 +0530

    ""

--- lib/main.dart
+++ lib/main.dart
@@ -20,11 +20,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: //ComboPage()
-          // CoursesCombo() //UploadComboScreen(courseId: 'kuhuk')
-          AdminDashBoard(),
-
-      // home: CoursesInfo(),
+      home: AdminDashBoard(),
     );
   }
 }
